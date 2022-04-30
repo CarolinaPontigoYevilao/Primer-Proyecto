3c2702857402a9c18302032a9c4ff83dfce52770
CarolinaPontigoYevilao <cpontigo@utem.cl>
2022-04-29 21:02:55 -0400

    creé mi primer script

---
+++ mi-primer-script.R
@@ -0,0 +1,27 @@
+
+#suma
+4+4
+
+#multiplicación
+3*5
+
+#división
+4/6
+
+#elevado
+3**2
+
+#Raiz
+4**1/2
+
+# se guarda en respuesta la operación 
+# 4 + 4 
+respuesta <- 4 + 4
+
+# muestra la información que uno decide 
+print(respuesta)
+
+
+# almacenando texto
+respuesta <- "Que tengan un bonito descanso"
+print(respuesta)
